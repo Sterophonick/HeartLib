1d14c879fa8c01c768d89def10e15f879d62df9b
Sterophonick <34801996+Sterophonick@users.noreply.github.com>
2018-02-18 18:21:39 -0700

    Delete demo.bat

--- demo.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-C:\devkitPro\devkitARM\bin\arm-none-eabi-gcc -g -MMD -MP -MF -Wall -Og -O1 -O2 -O3 -O -Os -ffast-math -mlong-calls -mcpu=arm7tdmi -mtune=arm7tdmi -fomit-frame-pointer -marm -mthumb -mthumb-interwork -c main.c
-C:\devkitPro\devkitARM\bin\arm-none-eabi-gcc -specs=gba.specs -mthumb -mthumb-interwork main.o libheart.a -o main.elf
-C:\devkitPro\devkitARM\bin\arm-none-eabi-objcopy -O binary main.elf main.gba
-C:\devkitPro\devkitARM\bin\gbafix main.gba
-pause
