0391a56f27a62e8fa41b31d76db779e9b918d809
Sterophonick <34801996+Sterophonick@users.noreply.github.com>
2019-12-05 14:13:21 -0700

    fx update

--- rewrite/include/hrt_fx.h
+++ rewrite/include/hrt_fx.h
@@ -8,6 +8,10 @@
 ivoid hrt_SetFXMode(u16 mode);
 ivoid hrt_SetFXLevel(u8 level);
 ivoid hrt_FXSetBlendMode(u8 mode);
+ivoid hrt_SetFXAlphaLevel(u8 src, u8 dst);
+ivoid hrt_FXToggleBackdrop(u8 target);
+ivoid hrt_FXToggleOBJ(u8 target);
+ivoid hrt_FXToggleBG(u8 layer, u8 target);
 
 //Macros
 #define FX_LAYER_BG(bg, target) 	1UL << (bg + (target*8))
@@ -29,6 +33,27 @@ ivoid hrt_FXSetBlendMode(u8 mode)
 {
 	REG_BLDCNT &= ~(0x00C0);
 	REG_BLDCNT |= (mode << 6);
+}
+
+ivoid hrt_SetFXAlphaLevel(u8 src, u8 dst)
+{
+    REG_BLDALPHA_L = src;
+    REG_BLDALPHA_H = dst;
+}
+
+ivoid hrt_FXToggleBackdrop(u8 target)
+{
+	REG_BLDCNT ^= 1UL << (5 + (target*8));
+}
+
+ivoid hrt_FXToggleOBJ(u8 target)
+{
+	REG_BLDCNT ^= 1UL << (4 + (target*8));
+}
+
+ivoid hrt_FXToggleBG(u8 layer, u8 target)
+{
+	REG_BLDCNT ^= 1UL << (layer + (target*8));
 }
 
 #endif
