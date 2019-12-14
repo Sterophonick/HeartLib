575353beb1ad360188cbb3224edd23b546bd9a6d
Sterophonick <34801996+Sterophonick@users.noreply.github.com>
2019-12-14 10:37:16 -0700

    update

--- src/pcx.c
+++ src/pcx.c
@@ -13,8 +13,7 @@
 *                      2017-2019 Sterophonick
 *                          For Tubooboo
 \*******************************************************************/
-#include "libheart.h"
-extern gba_system __hrt_system;
+#include "hrt_misc.h"
    typedef struct {
     char		manufacturer;
     char		version;
@@ -34,59 +33,56 @@ extern gba_system __hrt_system;
 
 void hrt_DecodePCX(const u8 *PCXBuffer, u16 * ScreenAddr, u16* Palette)
 {
-	if (__hrt_system.hrt_start) {
-		u8 Buf[240];
-		register int c, l, r, g, b;
-		register int i;
-		register u16 *Scrn = ScreenAddr;
-		register pcx_header *header = (pcx_header *)PCXBuffer;
-		register u8 *Data = (u8*)PCXBuffer + sizeof(pcx_header);
-		register int Width = (header->x2 - header->x1) + 1;
-		register int Height = (header->y2 - header->y1) + 1;
-		Width = ((Width + 1) >> 1) << 1;
-		if (Width > 240 || Height > 160) return;
-		register u8 *wptr = Buf;
-		register int j, k;
-		for (j = 0; j < Height; j++)
+	u8 Buf[240];
+	register int c, l, r, g, b;
+	register int i;
+	register u16 *Scrn = ScreenAddr;
+	register pcx_header *header = (pcx_header *)PCXBuffer;
+	register u8 *Data = (u8*)PCXBuffer + sizeof(pcx_header);
+	register int Width = (header->x2 - header->x1) + 1;
+	register int Height = (header->y2 - header->y1) + 1;
+	Width = ((Width + 1) >> 1) << 1;
+	if (Width > 240 || Height > 160) return;
+	register u8 *wptr = Buf;
+	register int j, k;
+	for (j = 0; j < Height; j++)
+	{
+		for (k = 0; k < Width;)
 		{
-			for (k = 0; k < Width;)
+			c = *(Data++);
+			if ((c & 0xC0) == 0xC0)
 			{
+				l = c & 0x3f;
 				c = *(Data++);
-				if ((c & 0xC0) == 0xC0)
-				{
-					l = c & 0x3f;
-					c = *(Data++);
-
-					for (i = 0; i < l; i++)
-					{
-						*(wptr++) = c;
-					}
-
-					k += l;
-				}
-				else
+				for (i = 0; i < l; i++)
 				{
 					*(wptr++) = c;
-					k++;
 				}
+
+				k += l;
 			}
-			register u16* ptr = (u16 *)Buf;
-			for (i = 0; i < Width >> 1; i++)
+			else
 			{
-				*(Scrn++) = *(ptr++);
+				*(wptr++) = c;
+				k++;
 			}
-			wptr = Buf;
 		}
-		Data++;
-		register u16 * GBA_Palette = Palette;
-		register u16 color;
-		for (i = 0; i < 256; i++)
+		register u16* ptr = (u16 *)Buf;
+		for (i = 0; i < Width >> 1; i++)
 		{
-			r = *(Data++);
-			g = *(Data++);
-			b = *(Data++);
-			color = ((r >> 3) | ((g >> 3) << 5) | ((b >> 3) << 10));
-			*(GBA_Palette++) = color;
+			*(Scrn++) = *(ptr++);
 		}
+		wptr = Buf;
+	}
+	Data++;
+	register u16 * GBA_Palette = Palette;
+	register u16 color;
+	for (i = 0; i < 256; i++)
+	{
+		r = *(Data++);
+		g = *(Data++);
+		b = *(Data++);
+		color = ((r >> 3) | ((g >> 3) << 5) | ((b >> 3) << 10));
+		*(GBA_Palette++) = color;
 	}
 }
