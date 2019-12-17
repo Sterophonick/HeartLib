69b0b061dec815325c46d47a8232b271ba977c86
Sterophonick <34801996+Sterophonick@users.noreply.github.com>
2019-12-17 07:33:34 -0700

    cleanup

--- include/hrt_text.h
+++ include/hrt_text.h
@@ -6,14 +6,9 @@
 #include <stdio.h>
 #include "hrt_types.h"
 
-//Structs
-
-//Macros
-
 //Functions
 HEART_API void hrt_ClearText();
 HEART_API void hrt_InitTextEngine(u8 bg);
 HEART_API void hrt_Print(u8 tx, u8 ty, char* str, ...);
-
 
 #endif
