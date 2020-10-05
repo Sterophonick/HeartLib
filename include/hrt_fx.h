322035571bb021e47cf22a4b10529ba0d895ad1e
Sterophonick <34801996+Sterophonick@users.noreply.github.com>
2020-10-05 10:42:48 -0700

    move fx funcs to inlines

--- include/hrt_fx.h
+++ include/hrt_fx.h
@@ -13,21 +13,74 @@ extern "C" {
 //Macros
 
 //Functions
-HEART_API void hrt_SetFXMode(u16 mode);
-HEART_API void hrt_SetFXLevel(u8 level);
-HEART_API void hrt_FXSetBlendMode(u8 mode);
-HEART_API void hrt_SetFXAlphaLevel(u8 src, u8 dst);
-HEART_API void hrt_FXToggleBackdrop(u8 target);
-HEART_API void hrt_FXToggleOBJ(u8 target);
-HEART_API void hrt_FXToggleBG(u8 layer, u8 target);
-HEART_API void hrt_FXEnableBackdrop(u8 target);
-HEART_API void hrt_FXDisableBackdrop(u8 target);
-HEART_API void hrt_FXEnableBG(u8 layer, u8 target);
-HEART_API void hrt_FXDisableBG(u8 layer, u8 target);
-HEART_API void hrt_FXEnableOBJ(u8 target);
-HEART_API void hrt_FXDisableOBJ(u8 target);
 
 //Inlines
+inline void hrt_SetFXMode(u16 mode)
+{
+	REG_BLDCNT = mode;
+}
+
+inline void hrt_SetFXLevel(u8 level)
+{
+	REG_BLDY = level;
+}
+
+inline void hrt_FXSetBlendMode(u8 mode)
+{
+	REG_BLDCNT &= ~(0x00C0);
+	REG_BLDCNT |= (mode << 6);
+}
+
+inline void hrt_SetFXAlphaLevel(u8 src, u8 dst)
+{
+    REG_BLDALPHA_L = src;
+    REG_BLDALPHA_H = dst;
+}
+
+inline void hrt_FXToggleBackdrop(u8 target)
+{
+	REG_BLDCNT ^= 1UL << (5 + (target*8));
+}
+
+inline void hrt_FXToggleOBJ(u8 target)
+{
+	REG_BLDCNT ^= 1UL << (4 + (target*8));
+}
+
+inline void hrt_FXToggleBG(u8 layer, u8 target)
+{
+	REG_BLDCNT ^= 1UL << (layer + (target*8));
+}
+
+inline void hrt_FXEnableBackdrop(u8 target)
+{
+	REG_BLDCNT |= (1UL << (5 + (target*8)));
+}
+
+inline void hrt_FXDisableBackdrop(u8 target)
+{
+	REG_BLDCNT &= ~(1UL << (5 + (target*8)));
+}
+
+inline void hrt_FXEnableBG(u8 layer, u8 target)
+{
+	REG_BLDCNT |= 1UL << (layer + (target*8));
+}
+
+inline void hrt_FXDisableBG(u8 layer, u8 target)
+{
+	REG_BLDCNT &= ~(1UL << (layer + (target*8)));
+}
+
+inline void hrt_FXEnableOBJ(u8 target)
+{
+	REG_BLDCNT |= (1UL << (4 + (target*8)));
+}
+
+inline void hrt_FXDisableOBJ(u8 target)
+{
+	REG_BLDCNT &= ~(1UL << (4 + (target*8)));
+}
 
 #endif
 
